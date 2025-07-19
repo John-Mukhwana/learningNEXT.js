1cdd40db3b93320cbd6e1b4aaca3c34184055170
John-Mukhwana <johnbradill67@gmail.com>
2025-07-19 17:54:35 +0300

    Fix typo in homepage title and add links to posts

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -5,10 +5,16 @@ export default function Home() {
   return (
     <main>
       <h1 className="text-3xl font-bold underline">
-        Welcome to our HomePag!
+        Welcome to our HomePage!
       </h1>
       <Link href="/about" className="text-blue-500 hover:underline">
         Go to About Page
+      </Link>
+      <Link href="/posts/1" className="text-blue-500 hover:underline ml-4">
+        View Post 1
+      </Link>
+      <Link href="/posts/2" className="text-blue-500 hover:underline ml-4">
+        View Post 2
       </Link>
     </main>
   );
